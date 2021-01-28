512e5605e8d0f16cfdb70131521f0870801d5c9d
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-01-28 17:23:37 -0600

    added documentaiton to main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Project:            Missionaries and Cannibals 
+Description:        Solve the MC puzzle using BFS, see README for further details.
+Contributors:       Blake Engelbrecht
+Date:               1/28/2021 
+"""
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
